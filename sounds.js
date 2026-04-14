09ddfcdb1ab5a8e291c423614ed08c5bcac60b4a
rorywalsh <rorywalsh@ear.ie>
2026-04-14 11:33:58 +0100

    Updating sound placement logic

---
+++ sounds.js
@@ -0,0 +1,7 @@
+// ============================================================
+// SOUNDS — this is the only file you need to edit
+// ============================================================
+function initSounds() {
+    let areaSounds = [];
+    areaSounds.push(new AreaSound({ file: "./audio/toneC3.ogg", x: -71.58, y: -15.07, z: -58.97, volume: 1, rolloff: 1.4, minDistance: 2, visible: false }));
+}
